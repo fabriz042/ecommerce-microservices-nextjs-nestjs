6d1359c29cc503e5b0f08b53a96b57a6e23966c8
Fabrizio <fabrizziotaco20@gmail.com>
2025-10-29 22:58:38 -0500

    feat: update CategoryPage to validate category and handle not found cases

--- apps/frontend/src/app/[lang]/products/[category]/page.tsx
+++ apps/frontend/src/app/[lang]/products/[category]/page.tsx
@@ -1,8 +1,20 @@
-export default async function CategoriaPage({
+import { getCategory } from "@/services/category/category.service";
+import { notFound } from "next/navigation";
+
+export default async function CategoryPage({
   params,
 }: {
-  params: Promise<{ categoria: string }>;
+  params: Promise<{ category: string }>;
 }) {
-  const { categoria } = await params;
-  return <h1>Categoría: {categoria}</h1>;
+  const { category } = await params;
+
+  const categories = await getCategory();
+
+  const validCategories = categories.map((cat) => cat.name.toLowerCase());
+
+  if (!validCategories.includes(category)) {
+    notFound();
+  }
+
+  return <h1 className="mt-[150px]">Categoría: {category}</h1>;
 }
